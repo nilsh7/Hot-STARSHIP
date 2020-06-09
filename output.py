0e9a738301b669a3d6c7dfaf84857b9f6d14d5ce
Nils Henkel <nils.henkel96@gmail.com>
2020-06-09 19:00:10 +0200

    Added plot function for beta Added temperature value at last boundary for adiabatic BC

--- output.py
+++ output.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotT(layers, Tnu, Tmap, t):
+
+def plotT(layers, Tnu, Tmap, t, inputvars):
+
     plt.clf()
     ztot = np.array([])
     Ttot = np.array([])
@@ -10,6 +12,13 @@ def plotT(layers, Tnu, Tmap, t):
             iLay = int(key[3:])
             ztot = np.append(ztot, layers[iLay].grid.zj)
             Ttot = np.append(Ttot, Tnu[Tmap[key]])
+            if "int" + key[3:] not in Tmap:
+                # Last layer
+                if inputvars.BCbackType == "adiabatic":
+                    ztot = np.append(ztot, layers[iLay].grid.zjp12[-1])
+                    Ttot = np.append(Ttot, Ttot[-1])
+                else:
+                    raise UserWarning("Unknown back BC %s" % inputvars.BCbackType)
         if key[0:3] == "int":
             iInt = int(key[3:])
             ztot = np.append(ztot, layers[iInt].grid.zjp12[-1])
@@ -17,5 +26,24 @@ def plotT(layers, Tnu, Tmap, t):
     plt.plot(ztot, Ttot, '-o', label='Numerical solution')
     plt.xlabel('z [m]')
     plt.ylabel('T [K]')
+    plt.title('Solution at t = %.3fs' % t)
+    plt.show()
+
+
+def plotBeta(layers, rhonu, rhomap, t):
+
+    plt.clf()
+    lay = layers[0]
+    mat = lay.material
+    gr = lay.grid
+    rhoj = rhonu[rhomap["lay0"]]
+    plt.clf()
+    beta = (mat.data.rhov0 - rhoj)/(mat.data.rhov0 - mat.data.rhoc0)
+    beta_int = beta[-2] + (beta[-1] - beta[-2])/(gr.zj[-1] - gr.zj[-2]) * (gr.zjp12[-1] - gr.zj[-2])
+    beta_tot = np.hstack((beta, beta_int))
+    z_tot = np.hstack((gr.zj, gr.zjp12[-1]))
+    plt.plot(z_tot, beta_tot, '-o', label='Numerical solution')
+    plt.xlabel('z [m]')
+    plt.ylabel('beta [-]')
     plt.title('Solution at t = %.3fs' % t)
     plt.show()
