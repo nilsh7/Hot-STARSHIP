959c0ad74b911429cd531513537f573727120371
Nils Henkel <nils.henkel96@gmail.com>
2020-11-14 17:18:04 +0100

    Add .nojekyll

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,7 @@ html_css_files = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_baseurl = '/build/html'
 
 
 #def setup(app):
